9e56f4be77bbddefa029225e2f471e17627c1f40
Karole <karolinanyambura@gmail.com>
2016-03-20 03:59:07 +0300

    grids components

--- clients/__code/ninja/app/ninja/play/caro.js
+++ clients/__code/ninja/app/ninja/play/caro.js
@@ -6,14 +6,63 @@ module.exports = {
     }
   },
   view:function(ctrl,args){
-    return m("div",[
+      return m("div",[
       m("h4","i am awesome"),
       m("ul",[
         ctrl.universities().map(function(uni){
-          return m("li",uni.name)
+      m("li",uni.name)
         })
       ]),
-      m(card)
+      m(card),
+      //m("div",{class:"card-panel"},[
+        m("div",{class:"divider"}),
+        m("div",{class:"row"},[
+          m("div",{class:"col l6"},[
+            m("div",{class:"card-panel"},"column 1")
+          ]),
+          m("div",{class:"col l6"},[
+            m("div",{class:"card-panel"},"column 2")
+        ]),
+      ]),
+
+      //]),
+
+      //m("div",{class:"card-panel"},[
+        m("div",{class:"divider"}),
+        m("div",{class:"row"},[
+          m("div",{class:"col l6"},[
+            m("div",{class:"card-panel"},"column 1")
+          ]),
+          m("div",{class:"col l6"},[
+            m("div",{class:"card-panel"},"columnl 2")
+          ])
+        ]),
+      //]),
+
+      //m("div",{class:"card-panel"},[
+        m("div",{class:"divider"}),
+        m("div",{class:"row"},[
+          m("div",{class:"col l4"},[
+            m("div",{class:"card-panel"},"column 1")
+          ]),
+          m("div",{class:"col l4"},[
+            m("div",{class:"card-panel"},"column 2")
+          ]),
+          m("div",{class:"col l4"},[
+            m("div",{class:"card-panel"},"column 3")
+          ]),
+
+      //]),
+
+      m("div",{class:"card-panel"},[
+        m("div",{class:"divider"}),
+        m("div",{class:"row"},[
+          m("div",{class:"col l3"},"coulmn 1"),
+          m("div",{class:"col l3"},"coulmn 2"),
+          m("div",{class:"col l3"},"coulmn 3"),
+          m("div",{class:"col l3"},"coulmn 4")
+        ])
+      ]),
     ])
   }
 }
--- clients/__code/ninja/app/ninja/play/caro_components/card.js
+++ clients/__code/ninja/app/ninja/play/caro_components/card.js
@@ -10,13 +10,14 @@ module.exports = {
     }
   },
   view:function(ctrl,args){
-    return m("div",{class:"card-panel"},[
+    return  m("div",{class:"card-panel"},[
       m("h1",{class:"center"},"my card component")
       ,m("div",{class:"divider"})
       ,m("div","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
       ,m("button",{
         onclick:function(){
           ctrl.talkSomethingElse();
+          m("div",{class:"divider"})
         }
       },"caro's button")
     ])
