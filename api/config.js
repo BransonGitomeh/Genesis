0bd8a08f554d4c488f25ce46d749b9004d7cc15f
BransonGitomeh <sirbranson@outlook.com>
2016-04-19 00:29:07 +0300

    removed localmongo ref , may be breaking deployment

--- api/config.js
+++ api/config.js
@@ -23,15 +23,7 @@ module.exports = {
     memory: {
       adapter: "memory"
     },
-    localMongo: {
-      adapter: 'mongo',
-      host: 'localhost', // defaults to `localhost` if omitted
-      port: 27017, // defaults to 27017 if omitted
-      // user: 'username_here', // or omit if not relevant
-      // password: 'password_here', // or omit if not relevant
-      // database: 'database_name_here' // or omit if not relevant
-    },
-
+    
     disk: {
       adapter: "disk"
     },
