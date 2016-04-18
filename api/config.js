15d2486e64347ee7bac7162bb34263576e1876f6
BransonGitomeh <sirbranson@outlook.com>
2016-04-18 13:59:57 +0300

    fixed typo connection string

--- api/config.js
+++ api/config.js
@@ -50,7 +50,7 @@ module.exports = {
       };
 
       if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-       var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+       var connection_string = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
           process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
           process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
           process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
